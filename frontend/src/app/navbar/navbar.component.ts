4c182dee8de3b8ff80392c0f2aa042ec91b722d3
Jaspreet Singh Kalsi <bugsbunny@MacBook-Pro-2.local>
2025-01-26 17:22:31 -0500

    remove unnessary code from frontend

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,9 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
-import {AuthService} from '../services/auth-service.service';
-import {SessionService} from '../services/session-service';
-import {NgIf} from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -13,32 +11,14 @@ import {NgIf} from '@angular/common';
     NgIf,
     RouterLinkActive
   ],
-  providers: [AuthService, SessionService],
+  providers: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
-  mode: string = '';
+export class NavbarComponent {
 
-  constructor(private _authService: AuthService, private _sessionService: SessionService) {
+  constructor() {
   }
 
-  ngOnInit() {
-    this.detectMode();
-  }
 
-  updateMode = (modeType: string) => {
-    this._sessionService.setMode(modeType);
-    this.mode = this._sessionService.getMode();
-  };
-
-  detectMode = () => {
-    this.mode = this._sessionService.getMode();
-  };
-
-  logout = () => {
-    this._sessionService.resetBase();
-    this._sessionService.resetMode();
-    this._authService.logout();
-  };
 }
